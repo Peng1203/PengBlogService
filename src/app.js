c7dbe1b4db6ae56227fede7d5d89b2e0dc8cfac7
Peng <17637473650@163.com>
2023-02-20 21:47:12 +0800

    Revert "全局使用 404 和 错误处理中间件"
    This reverts commit bbfc8a3ab0c9103e3289c4b95eb21b363fc43eca.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -19,15 +19,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 记录 HTTP 日志
-
-// 定义路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// 404中间件
-app.use(require('./middlewares/404Middleware'));
-// 错误处理中间件
-app.use(require('./middlewares/errorHandler'));
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
