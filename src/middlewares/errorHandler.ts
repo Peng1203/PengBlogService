683bc9f97d2958be99cb38aa187dd5862e64d14b
Peng <17637473650@163.com>
2023-02-26 18:29:00 +0800

    错误处理中间件修改

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -11,14 +11,19 @@ import { Request, Response, NextFunction } from 'express'
  * @returns {void}
  */
 
-function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): void {
+function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  console.log('触发错误中间件 -----')
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(500);
-  res.render('error');
+  res.status(500).send(`error: ${err.message}`)
 }
 
 export default errorHandler
