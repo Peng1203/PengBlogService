070a825b779e0ebc5d6b7380c3334d0cb5deadb9
Peng <17637473650@163.com>
2023-03-04 19:12:41 +0800

    完善校验DTO函数

--- src/helpers/validateParams.ts
+++ src/helpers/validateParams.ts
@@ -4,18 +4,20 @@ import MyError from './exceptionError'
 import { PARAMS_ERROR_CODE } from './errorCode'
 
 /**
- * 封装 validate校验DTO层方法 不抛出错误
+ * validate校验DTO层方法 不抛出错误 返回错误数组
  * @author Peng
  * @date 2023-03-04
  * @param {any} DTO: DTO层校验类
  * @param {any} params: 前端参数
- * @returns {any} 错误数组
+ * @returns {ValidationError[]} 错误数组
  */
 export async function validateDTO<T>(
   DTO: any,
   params: any
 ): Promise<ValidationError[]> {
   try {
+    // 限制前端传递多余参数
+    // whitelist 选项表示只接受被 class-validator 修饰的属性，而不接受未被修饰的属性； forbidNonWhitelisted 选项表示禁止接受未被修饰的属性，如果接收到未被修饰的属性，会抛出异常
     const options = {
       whitelist: true,
       forbidNonWhitelisted: true,
