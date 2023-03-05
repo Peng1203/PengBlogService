47590fddf7d00b7e849f36fc84ddf3ce7caede5b
Peng <17637473650@163.com>
2023-03-06 00:50:39 +0800

    表修改

--- sql/peng_blog_db.sql
+++ sql/peng_blog_db.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 80032
+ Source Server Version : 80032 (8.0.32)
  Source Host           : localhost:3306
  Source Schema         : peng_blog_db
 
  Target Server Type    : MySQL
- Target Server Version : 80032
+ Target Server Version : 80032 (8.0.32)
  File Encoding         : 65001
 
- Date: 27/02/2023 00:17:24
+ Date: 06/03/2023 00:49:38
 */
 
 SET NAMES utf8mb4;
@@ -22,20 +22,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id标识',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id标识',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文章标题',
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '作者',
-  `created_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `created_time` datetime NOT NULL COMMENT '创建时间',
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文章内容',
   `category` json NOT NULL COMMENT '文章分类',
   `tags` json NULL COMMENT '文章标签',
   `cover` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '文章封面',
-  `visits` int(0) NOT NULL DEFAULT 0 COMMENT '访问量',
-  `last_modify_time` datetime(0) NULL DEFAULT NULL COMMENT '上次修改时间',
+  `visits` int NOT NULL DEFAULT 0 COMMENT '访问量',
+  `last_modify_time` datetime NULL DEFAULT NULL COMMENT '上次修改时间',
   `modify_record` json NULL COMMENT '修改记录',
   `comments` json NULL COMMENT '评论',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for category
@@ -45,7 +45,7 @@ CREATE TABLE `category`  (
   `category_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '分类ID',
   `category_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '分类名称',
   PRIMARY KEY (`category_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for comment
@@ -53,49 +53,51 @@ CREATE TABLE `category`  (
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
   `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
-  `article_id` int(0) NOT NULL COMMENT '评论所属的文章ID',
+  `article_id` int NOT NULL COMMENT '评论所属的文章ID',
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '评论内容',
   `status` int(10) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000000 COMMENT '评论的状态 \r\n1 已发布 published\r\n2 待审核 pending\r\n3 已删除 deleted\r\n4 垃圾 spam\r\n5 置顶 top\r\n6 精华 essence',
-  `like_count` int(0) NOT NULL DEFAULT 0 COMMENT '点赞数',
+  `like_count` int NOT NULL DEFAULT 0 COMMENT '点赞数',
   `dislike_count` int(10) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000000 COMMENT '点踩数',
-  `created_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '评论时间',
+  `created_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '评论时间',
   `reply_list` json NULL COMMENT '回复数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for comment_reply
 -- ----------------------------
 DROP TABLE IF EXISTS `comment_reply`;
 CREATE TABLE `comment_reply`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `comment_id` int(0) NOT NULL COMMENT '评论的ID',
-  `article_id` int(0) NOT NULL COMMENT '文章ID',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `comment_id` int NOT NULL COMMENT '评论的ID',
+  `article_id` int NOT NULL COMMENT '文章ID',
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '回复内容',
-  `reply_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '回复时间',
-  `status` int(0) NOT NULL COMMENT '回复状态 \r\n1 已发布 published\r\n2 待审核 pending\r\n3 已删除 deleted',
+  `reply_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '回复时间',
+  `status` int NOT NULL COMMENT '回复状态 \r\n1 已发布 published\r\n2 待审核 pending\r\n3 已删除 deleted',
   `zan` int(10) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '点赞数',
   `cai` int(10) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '点踩数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for message_board
 -- ----------------------------
 DROP TABLE IF EXISTS `message_board`;
 CREATE TABLE `message_board`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for roles
+-- Table structure for role
 -- ----------------------------
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE `roles`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int NOT NULL AUTO_INCREMENT,
   `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `role_name`(`role_name` ASC) USING BTREE,
+  INDEX `id`(`id` ASC, `role_name` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -103,37 +105,43 @@ CREATE TABLE `roles`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`  (
-  `tag_id` int(0) NOT NULL,
+  `tag_id` int NOT NULL,
   `tag_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`tag_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for test
 -- ----------------------------
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `userName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `age` int(0) NOT NULL,
+  `age` int NOT NULL,
   `data` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `createdTime` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `createdTime` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1010 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `role_id` int NOT NULL COMMENT '角色ID',
+  `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '电子邮箱',
-  `state` int(0) NULL DEFAULT NULL COMMENT '用户状态',
-  `created_at` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '注册时间',
+  `state` int NULL DEFAULT NULL COMMENT '用户状态',
+  `created_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '注册时间',
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户头像',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `role_id`(`role_id` ASC) USING BTREE,
+  INDEX `role_name`(`role_name` ASC) USING BTREE,
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`role_name`) REFERENCES `role` (`role_name`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
