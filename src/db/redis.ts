bb3143ed6dda2f2b93156caabc8a214a611032dc
Peng <17637473650@163.com>
2023-03-10 14:14:12 +0800

    新增redis 封装Hash 操作方法

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -153,8 +153,8 @@ async function checkSetHasValue(
 ): Promise<boolean> {
   try {
     return !!(await redisClient.sismember(key, member))
-  } catch (e) {
-    console.log(e)
+  } catch (error) {
+    throw error
   }
 }
 
@@ -181,49 +181,107 @@ async function removeSetValues<T extends string | number>(
 }
 
 /**
+ * 设置指定Hash表数据 没有则自动创建
+ * @author Peng
+ * @date 2023-03-10
+ * @param {any} key:string
+ * @param {any} data:object
+ * @returns {number} 返回存储成功的 键值对 数量
+ */
+async function setHashCatch(key: string, data: object): Promise<number> {
+  try {
+    return await redisClient.hset(key, data)
+  } catch (error) {
+    throw error
+  }
+}
+
+/**
+ * 获取指定Hash表中指定key的数据 key不存在则返回null
+ * @author Peng
+ * @date 2023-03-10
+ * @param {any} key:string
+ * @param {any} hKey:string 表中的key
+ * @returns {string | null} 字符串结果 或者 null
+ */
+async function getKeyHash(key: string, hKey: string): Promise<string | null> {
+  try {
+    return await redisClient.hget(key, hKey)
+  } catch (error) {
+    throw error
+  }
+}
+
+/**
+ * 获取指定Hash表全部数据
+ * @author Peng
+ * @date 2023-03-10
+ * @param {any} key:string
+ * @returns {object} 查询结果对象 无则返回空{}
+ */
+async function getAllHash(key: string): Promise<object> {
+  try {
+    return await redisClient.hgetall(key)
+  } catch (error) {
+    throw error
+  }
+}
+
+/**
  * 测试
  * @author Peng
  * @date 2023-02-23
  * @returns {void}
  */
 async function test() {
-  console.log('redisClient -----', redisClient)
-  const status = await setCache('test', { name: '张三' }, 100)
-  // const status = await setCaches({ key4: '哈', key5: '哈哈', key6: '哈哈哈' }, 100)
-  console.log('设置缓存 -----', status) // OK
+  // const setHashResult = await redisClient.hset('testObj', {
+  //   name: 'zs',
+  //   age: '14',
+  // })
+  // console.log('setHashResult -----', setHashResult)
 
-  const res = await getCache('testSet')
-  console.log('获取缓存 -----', JSON.parse(res as any)) // 成功 value 失败 null
+  const getHashRes = await redisClient.hgetall('testObj1')
+  console.log('getHashRes -----', getHashRes)
+  // redisClient
+  //   .hget('testObj', 'name1')
+  //   .then((res) => console.log('res -----', res))
+  // console.log('redisClient -----', redisClient)
+  // const status = await setCache('test', { name: '张三' }, 100)
+  // // const status = await setCaches({ key4: '哈', key5: '哈哈', key6: '哈哈哈' }, 100)
+  // console.log('设置缓存 -----', status) // OK
 
-  const delStatus = await delCache('test1') // 删除 成功1 失败0
-  console.log('删除缓存 -----', delStatus)
+  // const res = await getCache('testSet')
+  // console.log('获取缓存 -----', JSON.parse(res as any)) // 成功 value 失败 null
 
-  const isExist = await isExists('key5') // 存在true 不存在false
-  console.log('isExist -----', isExist)
+  // const delStatus = await delCache('test1') // 删除 成功1 失败0
+  // console.log('删除缓存 -----', delStatus)
 
-  const result = await incrCounter('testCounter')
-  console.log('result -----', result)
+  // const isExist = await isExists('key5') // 存在true 不存在false
+  // console.log('isExist -----', isExist)
 
-  const testCounter = await getCache('testCounter')
-  console.log('testCounter -----', testCounter)
+  // const result = await incrCounter('testCounter')
+  // console.log('result -----', result)
 
-  const addStatus = await addToSet<number | string>(
-    'testSet',
-    JSON.stringify({ name: 'zs', age: 18 })
-  )
-  console.log('集合添加状态 -----', addStatus)
+  // const testCounter = await getCache('testCounter')
+  // console.log('testCounter -----', testCounter)
 
-  const setHasThisValue = await checkSetHasValue(
-    'testSet',
-    JSON.stringify({ name: 'zs', age: 18 })
-  )
-  console.log('集合中是否包含当前值? -----', setHasThisValue)
+  // const addStatus = await addToSet<number | string>(
+  //   'testSet',
+  //   JSON.stringify({ name: 'zs', age: 18 })
+  // )
+  // console.log('集合添加状态 -----', addStatus)
 
-  const delCount = await removeSetValues('testSet', 3, 4)
-  console.log('删除集合数 -----', delCount)
+  // const setHasThisValue = await checkSetHasValue(
+  //   'testSet',
+  //   JSON.stringify({ name: 'zs', age: 18 })
+  // )
+  // console.log('集合中是否包含当前值? -----', setHasThisValue)
 
-  const setVal = await getSet('testSet')
-  console.log('测试集合 -----', setVal)
+  // const delCount = await removeSetValues('testSet', 3, 4)
+  // console.log('删除集合数 -----', delCount)
+
+  // const setVal = await getSet('testSet')
+  // console.log('测试集合 -----', setVal)
 
   redisClient.quit()
 }
@@ -240,4 +298,7 @@ export {
   getSet,
   checkSetHasValue,
   removeSetValues,
+  setHashCatch,
+  getKeyHash,
+  getAllHash,
 }
