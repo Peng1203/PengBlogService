ab1b8c89fc8d59dbad9cf2e4826d68ba27803e17
Peng <17637473650@163.com>
2023-03-10 15:40:16 +0800

    修改用户模型字段名 新增角色ID字段

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -7,65 +7,81 @@ import sequelize from '../db/sequelize'
  * @author Peng
  * @date 2023-03-10
  */
-const UserModel = sequelize.define('User', {
-  id: {
-    type: DataTypes.BIGINT,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  userName: {
-    type: DataTypes.CHAR,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.CHAR,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.CHAR,
-  },
-  // 用户状态 1正常 2锁定 3封禁
-  state: {
-    type: DataTypes.INTEGER,
-    validate: {
-      min: 1,
-      max: 3,
+const UserModel = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    roleID: {
+      field: 'role_id',
+      type: DataTypes.BIGINT,
+    },
+    userName: {
+      field: 'user_name',
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.CHAR,
+    },
+    // 用户状态 1正常 2锁定 3封禁
+    state: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 3,
+      },
+    },
+    createdTime: {
+      field: 'created_time',
+      type: DataTypes.DATE,
+      allowNull: false,
+      // defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      defaultValue: dateTimeFormat(),
+      // 获取创建日期时格式化 返回
+      get() {
+        return dateTimeFormat(this.getDataValue('createdTime'))
+      },
+    },
+    updateTime: {
+      field: 'update_time',
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: dateTimeFormat(),
+      // 获取创建日期时格式化 返回
+      get() {
+        return dateTimeFormat(this.getDataValue('updateTime'))
+      },
+    },
+    // 用户头像
+    avatar: {
+      type: DataTypes.BLOB,
+    },
+    // 账户解封日期
+    unsealTime: {
+      field: 'unseal_time',
+      type: DataTypes.DATE,
+      // defaultValue: '',
+      get() {
+        const unsealTime = this.getDataValue('unsealTime')
+        if (!unsealTime) return unsealTime
+        return dateTimeFormat(unsealTime)
+      },
     },
   },
-  createdTime: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    // defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    defaultValue: dateTimeFormat(),
-    // 获取创建日期时格式化 返回
-    get() {
-      return dateTimeFormat(this.getDataValue('createdTime'))
-    },
-  },
-  updateTime: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: dateTimeFormat(),
-    // 获取创建日期时格式化 返回
-    get() {
-      return dateTimeFormat(this.getDataValue('updateTime'))
-    },
-  },
-  // 用户头像
-  avatar: {
-    type: DataTypes.BLOB,
-  },
-  // 账户解封日期
-  unsealingTime: {
-    type: DataTypes.DATE,
-    // defaultValue: '',
-    get() {
-      const unsealingTime = this.getDataValue('unsealingTime')
-      if (!unsealingTime) return unsealingTime
-      return dateTimeFormat(unsealingTime)
-    },
-  },
-})
+  // 模型配置属性
+  {
+    tableName: 'user',
+    timestamps: false,
+  }
+)
 
 export default UserModel
