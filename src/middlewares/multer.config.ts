4375a69313ea06fbd9c27fdc632386630170879b
Peng <17637473650@163.com>
2023-04-03 18:14:42 +0800

    multer中间件增加允许将文件缓存到内容内存中

--- src/middlewares/multer.config.ts
+++ src/middlewares/multer.config.ts
@@ -4,7 +4,7 @@ import multer from 'multer'
 import { Request } from 'express'
 
 // 配置上传
-const storage = multer.diskStorage({
+const storageToDisk = multer.diskStorage({
   // 上传文件的目录
   destination(req: Request, file, cb): void {
     // console.log('file -----', file)
@@ -14,11 +14,18 @@ const storage = multer.diskStorage({
   // 上传文件的名称
   filename(req: Request, file, cb): void {
     // 解决中文名乱码的问题
-    file.originalname = Buffer.from(file.originalname, "latin1").toString("utf8")
+    file.originalname = Buffer.from(file.originalname, 'latin1').toString(
+      'utf8'
+    )
     cb(null, file.originalname)
-  }
+  },
 })
-// multer 配置
-const upload = multer({ storage })
-// 调用 upload.single('file')
-export default upload
+// multer 上传到本地的资源服务 配置
+export const uploadToDisk = multer({ storage: storageToDisk })
+// 调用 uploadToDisk.single('file')
+
+// 使用内存暂存文件
+const storageToRAM = multer.memoryStorage()
+// 上传文件暂存到内存中 用于获取buffer 并存储到数据库中 当调用 req.file.buffer 则释放内存中的文件
+export const uploadToRAM = multer({ storage: storageToRAM })
+// 调用 uploadToRAM.single('file')
