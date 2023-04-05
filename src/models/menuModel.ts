d59996f262d8fb74ae481c88ebcb5f696fd108bf
Peng <17637473650@163.com>
2023-04-05 22:39:27 +0800

    创建菜单模型

---
+++ src/models/menuModel.ts
@@ -0,0 +1,89 @@
+import sequelize from '../db/sequelize'
+import { DataTypes, Sequelize } from 'sequelize'
+import { dateTimeFormat } from '../utils/moment'
+
+const MenuModel = sequelize.define(
+  'Menu',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    menuName: {
+      field: 'menu_name',
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    menuPath: {
+      field: 'menu_path',
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    menuURI: {
+      field: 'menu_uri',
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    menuIcon: {
+      field: 'menu_icon',
+      type: DataTypes.CHAR,
+      defaultValue: '',
+    },
+    // 父级ID参数 0为 一级菜单
+    parentId: {
+      field: 'parent_id',
+      type: DataTypes.BIGINT,
+      defaultValue: 0,
+    },
+    // 拥有菜单的角色id列表
+    roles: {
+      type: DataTypes.JSON,
+      defaultValue: () => [],
+    },
+    createdTime: {
+      field: 'created_time',
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      get() {
+        return dateTimeFormat(this.getDataValue('createdTime'))
+      },
+    },
+    updateTime: {
+      field: 'update_time',
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      get() {
+        return dateTimeFormat(this.getDataValue('updateTime'))
+      },
+    },
+  },
+  // 模型配置属性
+  {
+    tableName: 'menu',
+    timestamps: false,
+  }
+)
+
+async function test() {
+  // const findRes = await MenuModel.findAll()
+  // console.log(
+  //   'findRes -----',
+  //   findRes.map(res => res.toJSON())
+  // )
+  const addMenu = {
+    menuName: '个人中心',
+    menuPath: '/personal',
+    menuURI: 'personal',
+    parentId: 0,
+    roles: [],
+  }
+  const addRes = await MenuModel.create(addMenu)
+  console.log('addRes -----', addRes)
+}
+// test()
+
+export default MenuModel
