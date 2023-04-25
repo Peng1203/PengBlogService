ee956a2e665674c600e5144f0e1c465e90d5710d
Peng <17637473650@163.com>
2023-04-25 18:04:08 +0800

    验证码 token有效时间修改

--- src/configs/sign.ts
+++ src/configs/sign.ts
@@ -1,12 +1,12 @@
 // token签名私钥
 export const PRIVITE_KEY = 'peng_blog_1203'
-// token 有效时间 (秒) 3小时
-export const EXPIRESD = 60 * 60 * 3
+// token 有效时间 (秒) 6小时
+export const EXPIRESD = 60 * 60 * 6
 // token签名人
 export const ISSUER = 'Peng'
 
 // 验证码有效时间 (秒)
-export const CAPTCHA_VALID_TIME = 300
+export const CAPTCHA_VALID_TIME = 60
 
 // 登录最大试错数
 export const MAX_TRY_ERROR_COUNT = 5
